9705a03cd5c527675d2bc683d26657f867a8b2f8
Nurbek Erkulov <volukren@gmail.com>
2023-04-01 18:01:39 +0600

    - alacritty.yml

--- setup.sh
+++ setup.sh
@@ -3,6 +3,3 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/dev/my/dotfiles/vimrc ~/.vimrc
 ln -s ~/dev/my/dotfiles/zshrc ~/.zshrc
 ln -s ~/dev/my/cp-book/UltiSnips ~/.vim/
-mkdir -p ~/.config/alacritty/themes
-git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
-ln -s ~/dev/my/dotfiles/alacritty.yml ~/.alacritty.yml
